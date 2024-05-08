9cac8a48023a9e71f4fe4ee696afacd99fa4291b
georgeeflash <georgeeflash@gmail.com>
2024-05-08 13:16:03 +0100

    Added Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Flash Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://flash-dashboard'),
+};
 
 export default function RootLayout({
   children,
